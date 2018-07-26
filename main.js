13394511d95699e5add65246bd22b7516e664c0d
missionbit <missionbit@navajowhite-caption.local>
2018-07-26 10:25:50 -0700

    Untitled

--- main.js
+++ main.js
@@ -11,6 +11,86 @@ const game = new Phaser.Game({
   }
 });
 
-game.scene.add("play", Play);
+game.scene.add("lv1", Play({
+  npcs: [
+    {
+      x: 350,
+      y: 300,
+      image: "Person8",
+      data: "Mr. Green",
+      animation: "wave8"
+    },
+    {
+      x: 200,
+      y: 200,
+      image: "Person9",
+      data: "I am still mr lemon",
+      animation: "wave9"
+    },
+    {
+      x: 500,
+      y: 500,
+      image: "Person2",
+      data: "I am not mr lemon",
+      animation: "wave2"
+    },
+    {
+      x: 600,
+      y: 500,
+      image: "Person1",
+      data: "Hello",
+      animation: "wave1"
+    },
+  ],
+  portals: [
+    {
+      x: 400,
+      y: 300,
+      image: "plant",
+      location: "lv2"
+    }
+  ]
+}));
 
-game.scene.start("play");
+game.scene.add("lv2", Play({
+  npcs: [
+    {
+      x: 600,
+      y: 500,
+      image: "Person3",
+      data: "Hello",
+      animation: "wave3"
+    },
+    {
+      x: 100,
+      y: 100,
+      image: "Person4",
+      data: "Hello",
+      animation: "wave4"
+    },
+    {
+      x: 150,
+      y: 500,
+      image: "Person6",
+      data: "Hello",
+      animation: "wave6"
+    },
+    {
+      x: 200,
+      y: 200,
+      image: "Person7",
+      data: "Hello",
+      animation: "wave7"
+    },
+  ],
+  portals: [
+    {
+      x: 400,
+      y: 300,
+      image: "plant",
+      location: "lv1"
+    }
+  ]
+}))
+
+game.scene.start("lv1");
--- states/play.js
+++ states/play.js
@@ -1,38 +1,129 @@
 const playerSpeed = 300;
 
-export default class Play {
+export default config => class Play {
   init() {
     this.player = undefined;
     this.npcs = undefined;
+    this.portals = undefined;
+
     this.keys = undefined;
 
     this.textShowing = null;
     this.text = undefined;
   }
   preload() {
-    this.load.image("lemon", "./assets/lemon.jpg");
+    this.load.image("plant", "./assets/Plant1.png");
+    this.load.spritesheet("Person1", "./assets/Person1.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person2", "./assets/Person2.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person3", "./assets/Person3.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person4", "./assets/Person4.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person5", "./assets/Person5.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person6", "./assets/Person6.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person7", "./assets/Person7.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person8", "./assets/Person8.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person9", "./assets/Person9.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("Person10", "./assets/Person10.png", { frameWidth: 32, frameHeight: 32 });
+
+
+
   }
   create() {
-    this.player = this.physics.add.sprite(50, 50, "lemon");
+    this.player = this.physics.add.sprite(50, 50, "Person5");
     this.player.setCollideWorldBounds(true);
 
+    this.anims.create({
+      key: "wave1",
+      frames: this.anims.generateFrameNumbers("Person1", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
+    this.anims.create({
+      key: "wave2",
+      frames: this.anims.generateFrameNumbers("Person2", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
+    this.anims.create({
+      key: "wave3",
+      frames: this.anims.generateFrameNumbers("Person3", { start: 0, end: 0}),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: "wave4",
+      frames: this.anims.generateFrameNumbers("Person4", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
+    this.anims.create({
+      key: "wave5",
+      frames: this.anims.generateFrameNumbers("Person5", { start: 0, end: 0}),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: "wave6",
+      frames: this.anims.generateFrameNumbers("Person6", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: "wave7",
+      frames: this.anims.generateFrameNumbers("Person7", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
+    this.anims.create({
+      key: "wave8",
+      frames: this.anims.generateFrameNumbers("Person8", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
+    this.anims.create({
+      key: "wave9",
+      frames: this.anims.generateFrameNumbers("Person9", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: "wave10",
+      frames: this.anims.generateFrameNumbers("Person10", { start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1
+    })
+    
     this.keys = this.input.keyboard.addKeys({
       W: Phaser.Input.Keyboard.KeyCodes.W,
       A: Phaser.Input.Keyboard.KeyCodes.A,
       S: Phaser.Input.Keyboard.KeyCodes.S,
       D: Phaser.Input.Keyboard.KeyCodes.D,
-      Right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
 
       SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
     });
 
     this.npcs = this.physics.add.staticGroup();
 
-    this.npcs.create(200, 200, "lemon").setData("text", "Hello, I am a lemon");
+    (config.npcs || []).forEach(c => {
+      const npc = this.npcs.create(c.x, c.y, c.image);
+      npc.setData("text", c.data);
+      npc.anims.play(c.animation);
+    })
 
-    this.npcs
-      .create(400, 400, "lemon")
-      .setData("text", "Hello, I am also lemon");
+    this.portals = this.physics.add.staticGroup();
+
+    (config.portals || []).forEach(c => {
+      const portal = this.portals.create(c.x, c.y, c.image);
+      portal.setData("location", c.location);
+    })
 
     this.text = this.add.text(400, 500, "", {
       color: "white"
@@ -45,6 +136,9 @@ export default class Play {
     } else {
       this.text.setText(this.textShowing);
     }
+
+
+    this.player.anims.play("wave1", true)
 
     if (this.keys.W.isDown) {
       this.player.setVelocityY(-playerSpeed);
@@ -66,6 +160,10 @@ export default class Play {
 
     this.physics.overlap(this.player, this.npcs, (player, npc) => {
       this.textShowing = npc.getData("text");
+    });
+
+    this.physics.overlap(this.player, this.portals, (player, portal) => {
+      this.scene.start(portal.getData("location"))
     });
   }
 }
